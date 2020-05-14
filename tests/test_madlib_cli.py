ee6ddfe982c80f2878169c414123297fe041a711
Haley Griffin <haleygriffin708@gmail.comgit config --global user.name h-griffin>
2020-05-13 22:09:39 -0700

    function shells with comments

--- tests/test_madlib_cli.py
+++ tests/test_madlib_cli.py
@@ -1,5 +1,17 @@
 from madlib_cli import __version__
-
+from madlib_cli import madlib_cli
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_read_template():
+  # actual = read_template()
+  # expected = 
+  # assert actual = expeceted
+  pass
+
+def test_populate_template():
+  # actual = populate_template()
+  # expected = 
+  # assert actual = expeceted
+  pass
